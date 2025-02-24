028c3ba0b177cb2b5a5cf3a11b0bc147099680d5
blu145 <bluyt145@gmail.com>
2025-02-24 13:12:46 +0000

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,15 @@
+public class Main 
+{
+    public static void main(String[] args)
+    {
+        GameArena arena = new GameArena(600,600);
+        Ball b = new Ball(250,150,20,"GREEN");
+
+        arena.addBall(b);
+
+        while(true)
+        {
+            arena.pause();
+        }
+    }
+}
