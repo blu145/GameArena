f63be5c40f555ef1e3a7917a3c683c8b76c9831f
blu145 <bluyt145@gmail.com>
2025-02-28 20:05:40 +0000

    Update Enemy.java

--- Enemy.java
+++ Enemy.java
@@ -1,30 +1,37 @@
-public class Main 
-{
-    public static void main(String[] args)
-    {
-        GameArena arena = new GameArena(600,600);
-        Enemy one = new Enemy(250,150,20,"RED",1,5);
-        Enemy two = new Enemy(200,150,20,"RED",1,5);
-        Enemy three = new Enemy(0,0,20,"RED",1,5);
+public class Enemy extends Ball{
+    private int speed;
+    
+    //CONSTRUCTOR
+    public Enemy (double x, double y, double diameter, String col, int layer, int speed){
+        super(x,  y,  diameter, col);
+        this.speed=speed;
+    }
 
-        arena.addBall(one);
-        arena.addBall(two);
-        arena.addBall(three);
-
-        while(true)
-        {
-            int run = 1;
-            while(run==1){
-                one.move(one.randomX(), one.randomY());
-                arena.pause();
-                two.move(two.randomX(), two.randomY());
-                arena.pause();
-                three.move(three.randomX(), three.randomY());
-                arena.pause();
-            }
-            
-            
+    //MOVE RANDOMLY
+    public double randomX(){
+        double randomX = (double)(Math.random() * 250)-100;
+        if (this.getXPosition()+randomX>600){
+            randomX-=(double)(Math.random() *50)-200;
         }
+        return randomX;
+    }
+
+    public double randomY(){
+        double randomY = (double)(Math.random() * 250)-100;
+        if (this.getYPosition()+randomY>600){
+            randomY-=(double)(Math.random() *50)-200;
+        }
+        return randomY;
+    }
+
+
+    //ACCESSORS AND MUTATORS
+    public void setSpeed(int newspeed){
+        this.speed=newspeed;
+    }
+
+    public int getSpeed(){
+        return this.speed;
     }
 }
 
