5f756705f73ebb79022e219bc91f40a6c5936ae6
notXtr3 <e.a.mitchell1@lancaster.ac.uk>
2025-02-28 20:58:13 +0000

    Update Enemy.java
    YABABABDDOO

--- Enemy.java
+++ Enemy.java
@@ -1,27 +1,52 @@
 public class Enemy extends Ball{
-    private int speed;
+    public int speed;
+    public int stepCounter;
+    public double xDirection;
+    public double yDirection;
+
     
     //CONSTRUCTOR
     public Enemy (double x, double y, double diameter, String col, int layer, int speed){
         super(x,  y,  diameter, col);
         this.speed=speed;
+        this.stepCounter = 0;
     }
 
     //MOVE RANDOMLY
-    public double randomX(){
-        double randomX = (double)(Math.random() * 250)-100;
-        if (this.getXPosition()+randomX>600){
-            randomX-=(double)(Math.random() *50)-200;
+    public double directionRandom(){
+        // Generates 0 or 1 or 2, used to decide negative or positive in move
+        // or no move in case of 2
+        if (this.stepCounter < 5){
+            return 2;
         }
-        return randomX;
+        else{
+            double rand = (Math.random() * 1);
+            this.stepCounter = 0;
+            return rand;
+        }
     }
 
-    public double randomY(){
-        double randomY = (double)(Math.random() * 250)-100;
-        if (this.getYPosition()+randomY>600){
-            randomY-=(double)(Math.random() *50)-200;
+    public void moveEnemy(double direction, String axis){
+        if (axis=="x"){
+            // Moving in X axis
+            if (direction==0){
+                this.move(-5,0);
+            }
+            else if(direction==1){
+                this.move(5,0);
+            }
         }
-        return randomY;
+        else{
+            // Moving in the Y axis yara
+            if (direction==0){
+                this.move(0,-5);
+            }
+            else if(direction==1){
+                this.move(0,5);
+            }
+
+        }
+        this.stepCounter += 1;
     }
 
 
@@ -34,4 +59,3 @@ public class Enemy extends Ball{
         return this.speed;
     }
 }
-
