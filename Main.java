70e65c1248b552109d98610419298f4e33644491
notXtr3 <e.a.mitchell1@lancaster.ac.uk>
2025-02-28 20:58:45 +0000

    Update Main.java
    WOOOOOOOOOOOOOOOOOOO

--- Main.java
+++ Main.java
@@ -4,23 +4,18 @@ public class Main
     {
         GameArena arena = new GameArena(600,600);
         Enemy one = new Enemy(250,150,20,"RED",1,5);
-        Enemy two = new Enemy(200,150,20,"RED",1,5);
-        Enemy three = new Enemy(0,0,20,"RED",1,5);
 
         arena.addBall(one);
-        arena.addBall(two);
-        arena.addBall(three);
 
         while(true)
         {
-            int run = 1;
-            while(run==1){
-                one.move(one.randomX(), one.randomY());
-                arena.pause();
-                two.move(two.randomX(), two.randomY());
-                arena.pause();
-                three.move(three.randomX(), three.randomY());
-                arena.pause();
+            boolean run = true;
+            while(run){
+                one.xDirection = one.directionRandom();
+                one.yDirection = one.directionRandom();
+                one.moveEnemy(one.xDirection,"x");
+                one.moveEnemy(one.yDirection,"y");
+
             }
             
             
