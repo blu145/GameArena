e3d69d4406c43d7b57800268857dd6ac65ce1902
notXtr3 <e.a.mitchell1@lancaster.ac.uk>
2025-03-01 17:30:10 +0000

    Update Main.java
    arena.pause();
    changed speed in "one" decleration

--- Main.java
+++ Main.java
@@ -3,12 +3,13 @@ public class Main
     public static void main(String[] args)
     {
         GameArena arena = new GameArena(600,600);
-        Enemy one = new Enemy(250,150,20,"RED",1,5);
+        Enemy one = new Enemy(250,150,20,"RED",1,1000);
 
         arena.addBall(one);
 
         while(true)
         {
+            arena.pause();
             one.moveEnemy();
         }
     }
