e62234f50d98353323e169d535b0f38c477e56fe
notXtr3 <e.a.mitchell1@lancaster.ac.uk>
2025-03-01 17:29:16 +0000

    Update Enemy.java
    Commented the revised version

--- Enemy.java
+++ Enemy.java
@@ -47,3 +47,54 @@ public class Enemy extends Ball{
         return this.speed;
     }
 }
+
+
+
+
+/* REMADE, needs border correction
+public class Enemy extends Ball{
+    public int speed;
+    public int stepCounter;
+    public int xDir;
+    public int yDir;
+
+    
+    //CONSTRUCTOR
+    public Enemy (double x, double y, double diameter, String col, int layer, int speed){
+        super(x,  y,  diameter, col);
+        this.speed=speed;
+        this.stepCounter = 0;
+        this.xDir = 0;
+        this.yDir = 0;
+    }
+
+    //MOVE RANDOMLY
+    public int directionRandom(){
+        // Returns either -1,0,1
+        return (int) (Math.floor(Math.random() * 3) - 1);
+    }
+
+    public void moveEnemy(){
+        if (this.stepCounter<this.speed){
+            // Enemy hasn't moved [speed] steps yet
+            this.move(this.xDir*5,this.yDir*5);
+            this.stepCounter += 1;
+        }
+        this.xDir = directionRandom();
+        this.yDir = directionRandom();
+        this.stepCounter = 0;
+    }
+
+
+    //ACCESSORS AND MUTATORS
+    public void setSpeed(int newspeed){
+        this.speed=newspeed;
+    }
+
+    public int getSpeed(){
+        return this.speed;
+    }
+}
+
+
+*/
